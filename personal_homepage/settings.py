e25a7f0aed8d9f5691de5bc837295795ba025ead
juha.fonsen <juha.fonsen@gmail.com>
2022-01-24 22:55:36 +0200

    Use postgresql

--- personal_homepage/settings.py
+++ personal_homepage/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'personal_homepage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homepagedb',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
