748831eda2a1ad5e3258b4144112670070a1eef5
Juha Fonsen <juha.fonsen@gmail.com>
2022-02-17 21:49:44 +0200

    Static files

--- personal_homepage/urls.py
+++ personal_homepage/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import stat
 from django.contrib import admin
 from django.urls import path
 from jobs import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
