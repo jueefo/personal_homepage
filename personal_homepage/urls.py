9436c6d1c22486e868272759e6d6ffdb51d986a8
Juha Fonsen <juha.fonsen@gmail.com>
2022-02-19 14:36:05 +0200

    Starts details view

--- personal_homepage/urls.py
+++ personal_homepage/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('jobs/<int:job_id>', views.detail, name='detail'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
